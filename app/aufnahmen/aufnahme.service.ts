d22723cea3f64d138756b32f2a56ba4ef0f6e70a
dieterch <dieterchvatal@Dieters-MacBook-Pro.local>
2017-02-07 21:34:49 +0100

    mit mediabojekt

--- app/aufnahmen/aufnahme.service.ts
+++ app/aufnahmen/aufnahme.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AufnahmeService {
 	private _url = "http://jsonplaceholder.typicode.com/posts";
-	private _url1 = "http://192.168.11.8:8002/recordings.json?start=0&limit=10";
+	private _url1 = "http://192.168.11.8:8002/recordings.json?start=0&limit=100";
 
 	constructor(private _http: Http) {
 	}
@@ -14,7 +14,28 @@ export class AufnahmeService {
 		var url = this._url1;
 		// console.log(this._http.get(url))
 		return this._http.get(url)
-			.map(res => res.json());
+			.map(res => {
+				var myres = res.json();
+
+				// Sortieren nach titel
+				myres.recordings.sort((n1,n2) => {
+					if (n1.event_title > n2.event_title) {
+						return 1;
+					}
+					if (n1.event_title < n2.event_title) {
+						return -1;
+					}
+					return 0;
+				})
+
+				// Ein Feld ergänzen
+				myres.recordings.forEach(aufnahme => {
+					aufnahme.imageurl = "http://192.168.11.8:5100/images/" + aufnahme.name + ".jpg";
+					aufnahme.streamurl = "vlc-x-callback://x-callback-url/stream?url=http://192.168.11.8:3000/" + aufnahme.inode + ".rec";
+				});
+
+				return myres
+			});
 	}
 
 	getPosts(filter?) {
