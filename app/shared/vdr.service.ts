9be43540b028e1c536504d3dc3a0671e3ea4905d
dieterch <dieterchvatal@Dieters-MBP.chvatal.linux>
2017-03-08 22:17:23 +0100

    updates

--- app/shared/vdr.service.ts
+++ app/shared/vdr.service.ts
@@ -8,6 +8,8 @@ import * as _ from 'underscore';
 @Injectable()
 export class VdrService implements OnInit {
 	private _vdrurl = "http://192.168.11.8";
+	private _restproxyurl = this._vdrurl + ":8080";
+	// private _restproxyurl = "http://localhost:8080";
 	private _resturl = this._vdrurl + ":8002";
 	private _pyresturl = this._vdrurl + ":5100"; 
     private _keys = {
@@ -66,91 +68,24 @@ export class VdrService implements OnInit {
 // --------- Recordings Start -------------
 
 	getRecordingsCategories() {
-		var url = this._resturl + "/recordings.json?start=0&limit=0";
+		let url = this._restproxyurl+ "/categories";	
 		return this._http.get(url)
 			.map(res => {
-				var myres = res.json();
-				var _cat = [];
-
-                // aus allen Recordings die Kategorien (= Unterverzeichnisse) extrahieren
-				myres.recordings.forEach(rec => {
-					_cat = _.union(_cat, rec.name.split('~').slice(0,-1));
-					// console.log(rec.name, _cat)
-				});
-                
-                // nach Namen sortieren
-				return _cat.sort((n1,n2) => {
-					if (n1 > n2) {
-						return 1;
-					}
-					if (n1 < n2) {
-						return -1;
-					}
-					return 0;
-				});
-			}
-		);
+				return res.json().categories;
+			});	
 	}
 
-
 	getRecordings(query?, category?, sort?) {
-		let url = "http://192.168.11.8:8080/aufnahmen";
+		let url = this._restproxyurl+ "/aufnahmen";
 		let myquery = "";
-		if (query) { myquery = myquery + "&query=" + query;}
-		if (category) { myquery = myquery + "&category=" + category;}
-		if (sort) { myquery = myquery + "&sort=" + sort;}
+		if (query) 		{ myquery = myquery + "&query=" + query;}
+		if (category) 	{ myquery = myquery + "&category=" + category;}
+		if (sort) 		{ myquery = myquery + "&sort=" + sort;}
 		if (myquery != "") { url = url + "?" + myquery.slice(1);}
 		// console.log(url);
 		return this._http.get(url)
 			.map(res => res.json());
 	}
-
-	// getRecordings(filter?, category?) {
-	// 	var url = this._resturl + "/recordings.json?start=0&limit=0";
-	// 	// console.log(this._http.get(url))
-	// 	return this._http.get(url)
-	// 		.map(res => {
-
-	// 			// respose im json format lesen
-	// 			var myres = res.json();
-
-	// 			// myres nach category filtern
-	// 			if (category) {
-	// 				myres.recordings = myres.recordings.filter( rec => rec.name.indexOf(category) >=0 );
-	// 			}
-
-	// 			// myres filtern
-	// 			if (filter) {
-	// 				myres.recordings = myres.recordings.filter( rec => rec.event_title.indexOf(filter) >=0 );
-	// 			}
-
-	// 			/* Sortieren nach titel
-	// 			myres.recordings.sort((n1,n2) => {
-	// 				if (n1.event_title > n2.event_title) {
-	// 					return 1;
-	// 				}
-	// 				if (n1.event_title < n2.event_title) {
-	// 					return -1;
-	// 				}
-	// 				return 0;
-	// 			}) */
-
-	// 			// Sortieren Absteigend nach Datum
-	// 			myres.recordings.sort((n1,n2) => { return n2.event_start_time - n1.event_start_time })
-
-
-	// 			/*/ Felder ändern/ergänzen
-	// 			myres.recordings.forEach(aufnahme => {
-	// 				// aufnahme.imageurl = "http://192.168.11.8:5100/images/" + aufnahme.event_title + ".jpg";
-	// 				// aufnahme.streamurl = this.sanitizer.bypassSecurityTrustUrl("vlc-x-callback://x-callback-url/stream?url=http://192.168.11.8:3000/" + aufnahme.inode + ".rec");
-	// 				// aufnahme.openvlc = this.sanitizer.bypassSecurityTrustUrl("http://192.168.11.8:5100/playpc/" + aufnahme.number);
-	// 				//aufnahme.startonTv = this.sanitizer.bypassSecurityTrustUrl(this._url + "/recordings/play/" + encodeURIComponent(aufnahme.file_name));
-	// 				//aufnahme.startonTv = this.sanitizer.bypassSecurityTrustUrl(this._url + "/recordings/play" + aufnahme.file_name);
-	// 			}); */
-
-	// 			return myres
-	// 		});
-	// }
 
 	getRecording(Id){
 		return this._http.get(this.getAufnahmeUrl(Id))
@@ -161,8 +96,8 @@ export class VdrService implements OnInit {
 			});
 	}
 
-	private getAufnahmeUrl(Id){
-		return this._resturl + "/recordings.json?start=" + Id + "&limit=1";
+	private getAufnahmeUrl(Id?) {
+		return this._restproxyurl + '/aufnahmen/' + Id;
 	}
 // --------- Recordings End -------------
 
