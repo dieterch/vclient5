aad42c0c28abd3434bb24c5e5f900cd64fa52ed0
dieterch <dieterchvatal@Dieters-MBP.chvatal.linux>
2017-03-11 20:06:48 +0100

    update

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,11 +1,11 @@
 
 import { RouterModule  }     from '@angular/router';
-// import { HomeComponent }     from './home.component';
+import { HomeComponent }     from './home.component';
 import { AppComponent }     from './app.component';
 import { NotFoundComponent } from './not-found.component';
 
 export const routing = RouterModule.forRoot([
-	{ path: '', component: AppComponent },
+	{ path: '', component: HomeComponent },
     { path: 'not-found', component: NotFoundComponent },
 	{ path: '**', redirectTo: 'not-found' }
 ]);
